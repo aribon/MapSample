7e3a0291c3ec1797031dcf991c09802d4dff741e
Unknown <anthony.ribon@gmail.com>
2018-01-17 03:12:16 +0100

    test module address

--- app/src/main/java/me/aribon/mapsample/ui/main/MainActivity.java
+++ app/src/main/java/me/aribon/mapsample/ui/main/MainActivity.java
@@ -1,5 +1,15 @@
 package me.aribon.mapsample.ui.main;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.location.places.ui.SupportPlaceAutocompleteFragment;
+
 import me.aribon.mapsample.R;
 import me.aribon.mapsample.ui.base.BaseActivity;
 
